ed1a10a46f779b1cbcd3bf0c0e677a6137800c1c
Tomás <tomaspaivadelira1@gmail.com>
2023-06-27 13:44:32 -0300

    adicionei a função citation, fiz alterações no tamnhao do gráfico

--- vis1.py
+++ vis1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from bokeh.plotting import figure, show
-from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.models import ColumnDataSource, HoverTool, Label
 from bokeh.io import output_file
 from bokeh.transform import factor_cmap
 from bokeh.palettes import Category10
@@ -20,7 +20,7 @@ colors = factor_cmap("Company", palette=Category10[10], factors=companies)
 
 figure = figure(width=800, height=600)
 
-scatter_plot = figure.circle(x="Flavor", y="Acidity", source=coffee_data, size=8, alpha=0.9,color=colors)
+scatter_plot = figure.circle(x="Flavor", y="Acidity", source=coffee_data, size=8, alpha=0.7, color=colors)
 
 figure.xaxis.axis_label = "Sabor"
 figure.yaxis.axis_label = "Acidez"
@@ -33,6 +33,17 @@ tooltips = [("Flavor", "@Flavor"), ("Acidity", "@Acidity"), ("Company", "@Compan
 hover_tool = HoverTool(renderers=[scatter_plot], tooltips=tooltips)
 figure.add_tools(hover_tool)
 
+#labels = LabelSet(x='Flavor', y='Acidity', text='Company', polui demais o gráfico por isso vou usar o citation
+#              x_offset=5, y_offset=5, source=coffee_data)
+
+citation = Label(x=2, y=513, x_units='screen', y_units='screen', text='Identificação das companhias pelas cores',
+                 border_line_color='black', border_line_alpha=1.0,
+                 background_fill_color='white', background_fill_alpha=0.3, text_font_size='9pt')
+
+figure.add_layout(citation)
+
 show(figure)
+
+
 
 
