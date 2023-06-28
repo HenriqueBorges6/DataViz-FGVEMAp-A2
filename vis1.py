0d2fadc5d6a44ebe9879ba99c870fd1e5dc3a296
Henrique Borges Carvalho <126023248+HenriqueBorges6@users.noreply.github.com>
2023-06-28 19:51:14 -0300

    Adicionei jitter, fiz correção do alpha, e alguns estéticos

--- vis1.py
+++ vis1.py
@@ -1,40 +1,61 @@
 #Bibliotecas utilizadas
 import pandas as pd
+import numpy as np
 from bokeh.plotting import figure, show
-from myfunctions import cds_generator 
-from bokeh.models import ColumnDataSource
+from myfunctions import cds_generator
+from bokeh.models import HoverTool, ColumnDataSource
+from bokeh.io import curdoc
 
+
+#Aplicando um tema
+curdoc().theme = "light_minimal"
+
+#Utilizando a função para transformar o dataset em CDS
 dados = cds_generator("cleaned_coffee_dataset.csv")
 
+#Preparando os dados
 aroma = dados.data["Body"]
 sabor = dados.data["Flavor"]
 nota_geral = dados.data["Overall"]
 
-"""
-invertendo a escala da nota geral para, quanto menor a nota, mais transparente
-"""
-max_nota = max(nota_geral)
-nota_geral_invertida = [max_nota - nota for nota in nota_geral]
+#Fazendo um alpha para que quanto maior o Overall do café, menos trasparente apareça no plot
+alpha = (nota_geral - 6.0) / (8.5 - 6.0)
 
-#Fazendo o gráfico
-aroma_sabor = figure(title="Cheira bem, é bom",
+# Adicionar jitter às coordenadas dos pontos
+jitter_range = 0.03  
+aroma_jitter = np.random.uniform(-jitter_range, jitter_range, len(aroma))
+sabor_jitter = np.random.uniform(-jitter_range, jitter_range, len(sabor))
+aroma += aroma_jitter
+sabor += sabor_jitter
+#Usamos o jitter para conseguir ter uma melhor noção da quantidade de amostras
+
+
+#preparando o gráfico
+aroma_sabor = figure(title="Cheira bem, é bom", 
                      x_axis_label="Aroma",
                      y_axis_label="Sabor",
-                     width = 513,
-                     height= 483)
-
+                     width=513, 
+                     height=483
+                     )
 """
-Esse será um scater plot
-Escolhi essa cor marrom por conta do tema café
-Nele temos a relação entre o aroma do café e seu sabor
-Os pontos menos transparentes são os que receberam notas maiores na coluna "Overall"
+Será um scatter plot, a ideia é verificar a relação entre o aroma e o sabor,
+ junto a média nos outros aspectos.
 """
 aroma_sabor.circle(aroma, sabor,
                    fill_color="#6F4E37",
                    line_color="#6F4E37",
-                   size = 10,
-                   fill_alpha=nota_geral_invertida/max_nota, 
-                   legend_label="Café"
+                   fill_alpha=alpha, 
+                   legend_label="Café",
+                   size = 10
 )
+#personalizando um pouco o plot, retirando alguns elementos "desnecessários"
+aroma_sabor.toolbar.autohide = True 
+aroma_sabor.toolbar.logo = None # Não vejo necessidade em aparecer a logo do bokeh
+aroma_sabor.grid.visible = False 
+# Não vejo necessidade da grade de fundo. A posição e o alpha são suficientes
 
+# Movi a legenda pra esquerda, assim ela sobrepõem os pontos
+aroma_sabor.legend.location = "top_left"
+
+#função pra mostrar o gráfico
 show(aroma_sabor)
