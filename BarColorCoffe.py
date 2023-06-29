3f416ddf398c3295f6e251e7f5b14fe8a5d99176
Henrique Borges Carvalho <126023248+HenriqueBorges6@users.noreply.github.com>
2023-06-29 11:40:34 -0300

    Adicionei cores as barras e reduzi a quantidade de barras agruprando as com poucas o observações

--- BarColorCoffe.py
+++ BarColorCoffe.py
@@ -19,14 +19,23 @@ cor_cafe_coluna = 'Color'
 cor_cafe_series = pd.Series(dados.data[cor_cafe_coluna])
 cor_cafe_contagem = cor_cafe_series.value_counts()
 
+# Obtendo as 7 cores mais frequentes e agrupando as demais
+top_7_cores = cor_cafe_contagem[:6]
+outras_cores = cor_cafe_contagem[6:]
+total_outras_cores = outras_cores.sum()
+top_7_cores['Outras Cores'] = total_outras_cores
+
+#Criando cores para cada barra
+cores_personalizadas = ['#008000', '#0EBE45', '#1BC4B4', '#1771B4', '#ADFF2F', '#964B00', '#78ABAB']
+
 # Criando o gráfico de barras
-p = figure(x_range=cor_cafe_contagem.index.tolist(), height=350, title="Contagem de Cores de Café")
-p.vbar(x=cor_cafe_contagem.index.tolist(), top=cor_cafe_contagem.values, width=0.9,
-       fill_color=factor_cmap(cor_cafe_coluna, palette=Category10_10, factors=cor_cafe_contagem.index.tolist()))
+qtd_cores = figure(x_range=top_7_cores.index.tolist(), height=350, title="Contagem de Cores de Café")
+qtd_cores.vbar(x=top_7_cores.index.tolist(), top=top_7_cores.values, width=0.9,
+       fill_color=cores_personalizadas)
 
 # Configurando o eixo x
-p.xgrid.grid_line_color = None
-p.xaxis.major_label_orientation = 1.2
+qtd_cores.xgrid.grid_line_color = None
+qtd_cores.xaxis.major_label_orientation = 1.2
 
 # Exibindo o gráfico
-show(p)
+show(qtd_cores)
