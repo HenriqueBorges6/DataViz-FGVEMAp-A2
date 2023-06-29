31282706b4d99aebd3e735e1aaf4412e90d488b6
Tomás <tomaspaivadelira1@gmail.com>
2023-06-29 09:06:20 -0300

    adicionei o range function e ajeitei as legendas

--- vis2.py
+++ vis2.py
@@ -1,27 +1,57 @@
-# Para rodar o código: bokeh serve --show vis2.py    
 import pandas as pd
 import numpy as np
 from bokeh.plotting import figure, curdoc
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, HoverTool, RangeSlider, Button
+from bokeh.layouts import column
 
 df = pd.read_csv("cleaned_coffee_dataset.csv")
-country_counts = df['Country of Origin'].value_counts()
+country = df['Country of Origin'].value_counts()
 
-countries = country_counts.index.tolist()
-cont = country_counts.tolist()
+countries = country.index.tolist()
+cont = country.tolist()
 
-bar_plot = figure(x_range=countries, height=600, width=850)
+source = ColumnDataSource(data=dict(countries=countries, counts=cont))
 
-bar_plot.vbar(x=countries, top=cont, width=0.8)
+hover = HoverTool(
+    tooltips=[
+        ("País", "@countries"),
+        ("Contagem", "@counts")
+    ]
+)
+
+bar_plot = figure(x_range=countries, height=600, width=850, tools=[hover])
+bar_plot.vbar(x='countries', top='counts', width=0.8, source=source, color='navy')
 
 bar_plot.xaxis.major_label_orientation = 'vertical'
-
 bar_plot.grid.grid_line_color = "lightgray"
 bar_plot.grid.grid_line_alpha = 0.5
 bar_plot.xaxis.axis_line_width = 1.2
 bar_plot.yaxis.axis_line_width = 1.2
-
 bar_plot.xaxis.axis_label = "Países"
 bar_plot.yaxis.axis_label = "Contagem"
 bar_plot.title.text = "Contagem dos países"
-curdoc().add_root(bar_plot)
+bar_plot.title.align = "center"
+
+range_slider = RangeSlider(title="Faixa", start=0, end=10, value=(0, 10), step=1)
+
+reset_button = Button(label="Resetar", button_type="default")
+
+def reset():
+    range_slider.value = (0, 20)
+
+def filter_data(attr, old, new):
+    lower, upper = range_slider.value
+    filtered_data = df[df['Country of Origin'].isin(countries)]
+    filtered_data = filtered_data.groupby('Country of Origin').filter(lambda x: lower <= len(x) <= upper)
+    filtered_counts = filtered_data['Country of Origin'].value_counts()
+    filtered_countries = filtered_counts.index.tolist()
+    
+    source.data = dict(countries=filtered_countries, counts=filtered_counts.tolist())
+    bar_plot.x_range.factors = filtered_countries  
+    
+range_slider.on_change('value', filter_data)
+reset_button.on_click(reset)
+
+layout = column(bar_plot, range_slider, reset_button)
+curdoc().add_root(layout)
+
