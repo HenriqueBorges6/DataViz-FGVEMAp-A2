5f4bef4c1961c36f909bdbfcabb512c2dc3abeca
Tomás <tomaspaivadelira1@gmail.com>
2023-06-30 18:25:20 -0300

    pequenas mudancas no titulo e formatacao do código

--- tomas_vis1.py
+++ tomas_vis1.py
@@ -32,6 +32,8 @@ def scatter_plot(data_path):
     figure_plot.yaxis.axis_label = "Acidez"
     figure_plot.title.text = "Sabor x Acidez"
     figure_plot.title.align = "center"
+    figure_plot.title.text_font_size = "12pt"
+
 
     # Customizando as linhas e grids dos gráficos
     figure_plot.grid.grid_line_color = "lightgray"
@@ -68,5 +70,3 @@ def scatter_plot(data_path):
     # Exibe o gráfico no navegador
     curdoc().add_root(select)
     curdoc().add_root(figure_plot)
-
-
