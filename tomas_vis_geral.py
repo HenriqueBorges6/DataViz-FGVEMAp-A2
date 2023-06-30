6b44ed8606afa504c665a637215e79001aed3e8e
Tomás <99937483+TomasLira@users.noreply.github.com>
2023-06-30 18:12:37 -0300

    Add files via upload

---
+++ tomas_vis_geral.py
@@ -0,0 +1,8 @@
+# Para rodar o código: bokeh serve -- tomas_vis_geral.py      
+import tomas_vis1
+import tomas_vis2
+import tomas_vis3
+
+tomas_vis1.scatter_plot("cleaned_coffee_dataset.csv")
+tomas_vis2.bar_plot("cleaned_coffee_dataset.csv")
+tomas_vis3.box_plot("cleaned_coffee_dataset.csv")
